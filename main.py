7cf6c15954c13a82d2eb2cfa713ed23bc5b93432
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-02-11 11:43:40 +0300

    Версия 0.0.1

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
 # -*- coding: utf-8 -*-
+"""
+Симулятор дома. В доме живут люди и коты.
+Люди могут работать, отдыхать и т.д.
+Коты могут есть, спать и т.д.
+Нужно прожить 365 дней.
+"""
+__author__ = "UglyGhoulChrist"
+__version__ = "0.0.1"
+
 from random import randint
-
-# Необходимо создать класс кота. У кота есть аттрибуты - сытость и дом (в котором он живет).
-# Кот живет с человеком в доме.
-# Для кота дом характеризируется - миской для еды и грязью.
-# Изначально в доме нет еды для кота и нет грязи.
-
-# Человеку и коту надо вместе прожить 365 дней.
-
-# Усложненное задание (делать по желанию)
-# Создать несколько (2-3) котов и подселить их в дом к человеку.
-# Им всем вместе так же надо прожить 365 дней.
-
-# (Можно определить критическое количество котов, которое может прокормить человек...)
-
-# Реализуем модель человека.
-# Человек может есть, работать, играть, ходить в магазин.
-# У человека есть степень сытости, немного еды и денег.
-# Если сытость < 0 единиц, человек умирает.
-# Человеку надо прожить 365 дней.
 from termcolor import cprint
 
 
 class House:
+    """
+    Класс Дом. В нём есть деньги на тумбочке, еда в холодильнике. Дом характеризуется чистотой.
+    """
 
     def __init__(self):
-        self.food = 50  # Еда для человека
-        self.cat_food = 50  # Еда для кота
-        self.money = 100  # Деньги
-        self.dirt = 0  # Степень загрязненности дома
+        self.food = 50
+        self.cat_food = 50
+        self.money = 100
+        self.dirt = 0
 
     def __str__(self):
-        return 'В доме еды для человека осталось {}, еды для кота осталось {},' \
-               ' денег осталось {}, степень загрязненности {}.' \
+        return 'В холодильнике еды для человека осталось {}, еды для кота осталось {},' \
+               ' на тумбочке денег осталось {}, степень загрязненности дома {}.' \
                ''.format(self.food, self.cat_food, self.money, self.dirt)
 
     def dirt(self):
@@ -43,6 +36,10 @@ class House:
 
 
 class Man:
+    """
+    Класс человек.Он может есть, работать, играть, убираться в доме, ходить в магазин.
+    У человека есть степень сытости, если сытость < 0 единиц, человек умирает.
+    """
 
     def __init__(self, name):
         self.name = name
@@ -90,10 +87,9 @@ class Man:
         self.fullness -= 10
         cprint('{} въехал в дом'.format(self.name), color='cyan')
 
-    #   убраться в доме - степень грязи в доме уменьшается на 100, сытость у человека уменьшается на 20.
     def clear_house(self):
         self.fullness -= 20
-        House.clean(self=my_sweet_home)
+        House.clean(self=sweet_home)
 
     def act(self):
         if self.fullness <= 0:
@@ -115,11 +111,14 @@ class Man:
         else:
             self.watch_MTV()
 
-        # Доработать класс человека, добавив методы
-        # подобрать кота - у кота появляется дом.
-
 
 class Cat:
+    """
+    Класс кот. У кота есть аттрибуты - сытость и дом (в котором он живет).
+    Кот живет с человеком в доме.
+    Для кота дом характеризируется - миской для еды и грязью.
+    Изначально в доме нет грязи.
+    """
 
     def __init__(self, name):
         self.cat_name = name
@@ -134,8 +133,6 @@ class Cat:
         self.house = house
         cprint('{} пришёл с помойки'.format(self.cat_name), color='cyan')
 
-    # Кот ест - сытость увеличивается на 20,
-    # кошачья еда в доме уменьшается на 10.
     def cat_eat(self):
         if self.house.cat_food >= 10:
             cprint('{} поел'.format(self.cat_name), color='yellow')
@@ -144,17 +141,14 @@ class Cat:
         else:
             cprint('{} нет еды'.format(self.cat_name), color='red')
 
-    # Кот спит - сытость уменьшается на 10
     def cat_sleep(self):
         cprint('{} спит'.format(self.cat_name), color='yellow')
         self.cat_fullness -= 10
 
-    # Кот дерет обои - сытость уменьшается на 10,
-    # степень грязи в доме увеличивается на 5
     def cat_wallpaper(self):
         cprint('{} дерет обои'.format(self.cat_name), color='red')
         self.cat_fullness -= 10
-        House.dirt(self=my_sweet_home)
+        House.dirt(self=sweet_home)
 
     def cat_act(self):
         if self.cat_fullness <= 0:
@@ -173,37 +167,34 @@ class Cat:
             print("Что-то пошло не так...")
 
 
-citizens = [
+# Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
+people = [
     Man(name='Бивис'),
     Man(name='Батхед'),
 ]
 
+# Создадим двух котов, живущих в одном доме с Бивисом и Батхедом.
 cats = [
-    Cat(name='Кот Белка'),
-    Cat(name='Кот Стрелка')
+    Cat(name='Барсик'),
+    Cat(name='Рыжик'),
 ]
 
-my_sweet_home = House()
-for citisen in citizens:
-    citisen.go_to_the_house(house=my_sweet_home)
-
-for cat in cats:
-    cat.go_to_the_house(house=my_sweet_home)
-
-for day in range(1, 366):
-    print('================ день {} =================='.format(day))
-    for citisen in citizens:
-        citisen.act()
-
+if __name__ == '__main__':
+    sweet_home = House()
+    for man in people:
+        man.go_to_the_house(house=sweet_home)
     for cat in cats:
-        cat.cat_act()
-    print('--- в конце дня ---')
-    for citisen in citizens:
-        print(citisen)
-    for cat in cats:
-        print(cat)
-    print(my_sweet_home)
+        cat.go_to_the_house(house=sweet_home)
 
-# Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
-# Нужен класс Дом, в нем должн быть холодильник с едой и тумбочка с деньгами
-# Еда пусть хранится в холодильнике в доме, а деньги - в тумбочке.
+    for day in range(1, 366):
+        print('================ день {} =================='.format(day))
+        for man in people:
+            man.act()
+        for cat in cats:
+            cat.cat_act()
+        print('--- в конце дня ---')
+        for man in people:
+            print(man)
+        for cat in cats:
+            print(cat)
+        print(sweet_home)
